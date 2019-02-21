773de7af54838c60b3584b2948e12361cf17c6b1
BasileoLuv <44769194+BasileoLuv@users.noreply.github.com>
2019-02-21 13:49:11 +0300

    Update artifactversion.sh

--- artifactversion.sh
+++ artifactversion.sh
@@ -14,4 +14,5 @@ funk="${major_version}.${minor_version}.$(( ${hotfix_version}+1))"
 sed "s/version=.*/version='$funk-SNAPSHOT',/g"  setup.py >2.txt
 mv 2.txt setup.py
 rm 1.txt
+git add .
 git commit -m "updated version"
